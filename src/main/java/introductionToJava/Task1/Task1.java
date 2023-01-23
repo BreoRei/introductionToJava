8d733f83e66e02a343c170011b2f844eadf82ad1
BreoRei <112073801+BreoRei@users.noreply.github.com>
2023-01-23 22:46:19 +0500

    Update Task1.java

--- src/main/java/introductionToJava/Task1/Task1.java
+++ src/main/java/introductionToJava/Task1/Task1.java
@@ -1,6 +1,5 @@
-package Task1;
+package introductionToJava.Task1;
 
-import java.util.Arrays;
 import java.util.Random;
 public class Task1 {
     public static void main(String[] args) {
@@ -43,8 +42,3 @@ public class Task1 {
         }
     }
 }
-
-
-
-
-
